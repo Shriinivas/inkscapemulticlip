41663a4cab9f99f51b55d5ff231ea8f7aeb054d7
Shrinivas Kulkarni <shrinivk@gmail.com>
2024-07-15 10:25:58 +0530

    Multi-clip objects other than image also

--- README.md
+++ README.md
@@ -1,41 +1,39 @@
-# Multi-clip Image Extension for Inkscape
+# Multi-clip Extension for Inkscape
 
 <p align="center">
   <img src="https://github.com/Shriinivas/etc/blob/master/inkscapemulticlip/horse.gif" alt="Demo"/>
 </p>
 
-The Multi-clip Image extension for Inkscape allows users to use selected shapes to create clipping regions on a selected image. This extension adds functionality to manipulate images with various clipping paths, optionally delete the clipping paths, group clipped parts, add them below the path, and even animate the clipping results.
+The Multi-clip extension for Inkscape allows users to apply the top selected objects as clipping regions on the lowest object (target) within the selection. It also has options to delete the clipping objects, group them, add them below the target, and animate the clipped results.
 
 ## Installation
 
-To install the Multi-clip Image extension, follow these steps:
+To install the Multi-clip extension, follow these steps:
 
-1. **Download the Extension**: Clone this repository or download the ZIP file and extract its contents.
-2. **Locate Your Inkscape Extensions Directory**:
+1. **Locate Your Inkscape Extensions Directory**:
    - You will find this in the 'User Extensions' option under the System section at Edit-Preferences in Inkscape.
-3. **Copy Files**: Copy the `multiclip.inx` and `multiclip.py` files into your Inkscape extensions directory.
-4. **Restart Inkscape**: Close and reopen Inkscape to load the new extension.
+2. **Download the Extension**: Download the ZIP file and extract `multiclip.inx` and `multiclip.py` files into your Inkscape extensions directory.
+3. **Restart Inkscape**: Close and reopen Inkscape to load the new extension.
 
 ## Usage
 
-To use the Multi-clip Image extension:
+To use the Multi-clip extension:
 
 1. **Open Inkscape**: Start Inkscape and open your project.
 2. **Select Elements**:
-   - Select the image you want to clip.
-   - Hold down `Shift` and select the shapes that will define the clipping regions.
-   - The position of image and the shapes in the layer don't matter, all the shape regions are applied to the first image within the selection
+   - Make sure the target object is placed lowest in the selection (you can select just the target and press Page Down multiple times to make sure it's placed below all the clipping objects)
+   - Select the target and clipping objects
 3. **Activate the Extension**:
-   - Navigate to `Extensions` > `Generate from Path` > `Multi-clip Image` to open the extension dialog.
+   - Navigate to `Extensions` > `Generate from Path` > `Multi-clip` to open the extension dialog.
 4. **Configure Options**:
-   - Set the options according to your needs (detailed below).
+   - Set the options according to your needs (details below).
    - Click `Apply` to perform the clipping operation.
 
 ## Options
 
-- **Delete Selection**: If checked, the selected shapes used for clipping will be deleted after the operation.
-- **Group with Shape**: If enabled, each clipped part of the image will be grouped with the corresponding shape used for clipping. This option is ignored if 'Delete Selection' is checked.
-- **Add Below**: If enabled, the clipped image will be added below the path in the document's layer stack. This option is ignored if 'Delete Selection' is checked.
+- **Delete Clipping Objects**: Uncheck this to keep the clipping objects after applying the clipping.
+- **Grouping**: Select 'Group with Target Part' or 'Keep Separate' to group or not group the clipping object with target part; this is considered only if 'Delete Clipping Objects' is unchecked.
+- **Clipping Object Position**: Select 'Below' or 'Above' to place the clipping object above or below the target part; this is considered only if 'Delete Clipping Objects' is unchecked.
 - **Animate**: Adds a simple SVG translation animation to each clipped part, moving it from a random position to its current position. This feature can only be observed when the SVG is viewed in a browser.
 
 ## [Video Tutorial](https://youtu.be/2XEb5q-T2xY)
--- multiclip.py
+++ multiclip.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 """
-Inkscape extension to create clip regions from selected image and shapes
+Inkscape extension to create clip an objects with objects placed above
 Tested with Inkscape version 1.3
 
-Copyright (C) 2024  Shrinivas Kulkarni (khemadeva@gmail.com)
+Author: Shrinivas Kulkarni (khemadeva@gmail.com)
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -20,34 +20,29 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 """
 
-import inkex
-from lxml import etree
-from inkex import Transform
-import random
+import inkex, random
+from lxml import etree  # type: ignore
 
 
 class MulticlipEffect(inkex.Effect):
     def __init__(self):
-        """Initialize the Multi-clip extension with command-line arguments."""
         inkex.Effect.__init__(self)
         arg = self.arg_parser.add_argument
-        arg(
-            "--tab", help="Select Options"
-        )  # Option tab in the GUI (not functional, just for layout grouping)
+        arg("--tab", help="Select Options")
         arg(
             "--delete_original",
             type=inkex.Boolean,
             help="Delete selected paths",
         )
         arg(
-            "--add_below",
-            type=inkex.Boolean,
-            help="Add clipped image below path",
+            "--position",
+            type=str,
+            help="Add clipped part below or above shape (ignored if 'Delete Selection' is checked)",
         )
         arg(
-            "--group_with_shape",
-            type=inkex.Boolean,
-            help="Group clipped part with path",
+            "--grouping",
+            type=str,
+            help="Whether to group clipped part with its clipping object (ignored if 'Delete Clipping Objects' is checked)",
         )
         arg(
             "--animate",
@@ -55,70 +50,15 @@ class MulticlipEffect(inkex.Effect):
             help="Add simple animation",
         )
 
-    def get_transform(self, elem, transform=Transform()):
-        """
-        Recursively accumulate the transformation matrix from the given element up to the root.
-
-        Args:
-            elem (lxml.etree.Element): The SVG element from which to start accumulating transformations.
-            transform (inkex.Transform): The initial transformation matrix (defaults to identity matrix).
-
-        Returns:
-            inkex.Transform: The accumulated transformation matrix.
-        """
-        transform = Transform(elem.get("transform")) @ transform
-        parent = elem.getparent()
-        if parent is not None:
-            return transform
-        return self.get_transform(parent, transform)
-
-    def add_group(self, parent, label):
-        """
-        Add a new group under the specified parent element and set its label.
-
-        Args:
-            parent (lxml.etree.Element): The parent element under which to add the group.
-            label (str): The label for the new group, which appears in Inkscape's object properties.
-
-        Returns:
-            lxml.etree.Element: The newly created group element.
-        """
-        group = etree.SubElement(parent, "g")
-        group.set(inkex.addNS("label", "inkscape"), label)
-        return group
-
-    def add_clip_path(self, defs, clipping_elem):
-        """
-        Create a clip path in the SVG 'defs' section using the specified clipping element.
-
-        Args:
-            defs (lxml.etree.Element): The 'defs' element of the SVG where the clip path is to be added.
-            clipping_elem (inkex.PathElement): The element used to define the shape of the clip path.
-
-        Returns:
-            lxml.etree.Element: The newly created clipPath element.
-        """
-        clip_path = etree.SubElement(defs, inkex.addNS("clipPath", "svg"))
-        path = etree.SubElement(clip_path, inkex.addNS("path", "svg"))
-        # Transform the path of the clipping element according to its cumulative transformation
-        tpath = clipping_elem.path.transform(self.get_transform(clipping_elem))
-        path.set("d", str(tpath))
-        return clip_path
+    def add_clip_path(self, clipping_elem):
+        clip_path_elem = self.svg.defs.add(inkex.ClipPath())
+        path_elem = clip_path_elem.add(inkex.PathElement())
+        path_elem.path = clipping_elem.path.transform(
+            clipping_elem.composed_transform()
+        )
+        return clip_path_elem
 
     def add_translate_anim(self, anim_parent, extent_min, extent_max, anim_dur):
-        """
-        Add a translate animation to the specified parent element, moving the element from a
-        random starting point to the origin.
-
-        Args:
-            anim_parent (lxml.etree.Element): The SVG element to which the animation is added.
-            extent_min (int): The minimum extent of the random start position.
-            extent_max (int): The maximum extent of the random start position.
-            anim_dur (int): The duration of the animation in seconds.
-
-        Returns:
-            lxml.etree.Element: The newly created animateTransform element.
-        """
         anim_trans = etree.SubElement(
             anim_parent, inkex.addNS("animateTransform", "svg")
         )
@@ -136,14 +76,10 @@ class MulticlipEffect(inkex.Effect):
         return anim_trans
 
     def effect(self):
-        """
-        Execute the effect of the Multi-clip extension which clips selected image with shapes and applies additional effects like grouping and animation.
-        """
-
         delete_original = self.options.delete_original
-        group_with_shape = self.options.group_with_shape
+        grouping = self.options.grouping
         animate = self.options.animate
-        add_below = self.options.add_below
+        position = self.options.position
 
         # Constants for animation bounds and duration
         anim_extent_min = -150
@@ -151,54 +87,51 @@ class MulticlipEffect(inkex.Effect):
         anim_dur = 1
 
         to_remove = set()
-        defs = self.svg.getElement("//svg:defs")
-        layer = self.document.getroot().get_current_layer()
-        img_elems = self.svg.selection.filter(inkex.Image)
-        if len(img_elems) == 0:
-            inkex.errormsg("Please select at least one image")
-            return
+        layer = self.svg.get_current_layer()
 
-        # Consider only the first image element in the selection
-        img_elem = img_elems[0]
-        img_id = img_elem.get_id()
+        elems = self.svg.selection.rendering_order()
+        target_elem = elems.pop(0)
+        target_id = target_elem.get_id()
         first_elem = True
 
-        # Iterate over selected elements to create clip paths and apply effects
-        for clipping_elem in self.svg.selection:
-            if isinstance(clipping_elem, inkex.Image):
+        for clipping_elem in elems:
+            if not clipping_elem.path:
                 continue
-            clip_path_id = self.add_clip_path(defs, clipping_elem).get_id()
-
-            # Determine the parent for the new 'use' element based on user options
-            # (create group if necessary)
+            clip_path_id = self.add_clip_path(clipping_elem).get_id()
             parent = layer if delete_original else clipping_elem.getparent()
-            if group_with_shape and not delete_original:
-                parent = self.add_group(parent, f"{clipping_elem.get_id()}-group")
-                parent.insert(0, clipping_elem)
+            if grouping == "group" and not delete_original:
+                group = inkex.Group()
+                group.set(
+                    inkex.addNS("label", "inkscape"), f"{clipping_elem.get_id()}-group"
+                )
+                clipping_elem.addprevious(group)
+                parent.remove(clipping_elem)
+                group.add(clipping_elem)
 
-            # If it's the first clipping element, append (move) the image to defs so that
+            # If it's the first clipping element, append (move) the target to defs so that
             # it can be referred by other 'use' elements ('original' for the 'clones')
             if first_elem:
-                defs.append(img_elem)
+                target_elem.getparent().remove(target_elem)
+                self.svg.defs.add(target_elem)
 
-            # Create a 'use' (clone) element to apply the clip path to the image
-            image = etree.SubElement(parent, inkex.addNS("use", "svg"))
-            image.set("xlink:href", f"#{img_id}")
-            image.set("clip-path", f"url(#{clip_path_id})")
+            # Create a 'use' (clone) element to apply the clip path to the object
+            target_clone = parent.add(inkex.Use())
+            target_clone.set("xlink:href", f"#{target_id}")
+            target_clone.set("clip-path", f"url(#{clip_path_id})")
             if not delete_original:
-                if add_below:
-                    clipping_elem.addprevious(image)
+                if position == "above":
+                    clipping_elem.addprevious(target_clone)
                 else:
-                    clipping_elem.addnext(image)
+                    clipping_elem.addnext(target_clone)
 
             if animate:
-                anim_parent = parent if group_with_shape else image
+                anim_parent = parent if grouping == "group" else target_clone
                 self.add_translate_anim(
                     anim_parent, anim_extent_min, anim_extent_max, anim_dur
                 )
-            first_elem = False
 
             to_remove.add(clipping_elem)
+            first_elem = False
 
         if delete_original:
             for clipping_elem in to_remove:
